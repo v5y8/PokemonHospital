bdceeac03d364bac3acd36836391be9f40b5a56e
v5y8 <jamesliu1991@hotmail.com>
2016-11-11 14:32:44 -0800

    oops. NOW sql script works

--- DB_setup.sql
+++ DB_setup.sql
@@ -1,3 +1,15 @@
+/*resets database by nuking any existing ones*/
+drop table trainer cascade constraints;
+drop table pokemonName cascade constraints;
+drop table pokemonBelongs cascade constraints;
+drop table professorTrades cascade constraints;
+drop table healPokemon cascade constraints;
+drop table nurse cascade constraints;
+drop table hospital cascade constraints;
+drop table incubation cascade constraints;
+drop table incubator cascade constraints;
+drop table eggDeposit cascade constraints;
+
 Create table trainer(
 	trainer_id int not null PRIMARY KEY,
 	trainer_name varchar(55) not null);
@@ -12,49 +24,66 @@ Create table pokemonName(
 Create table pokemonBelongs(
 	pid int not null,
 	trainer_id int, 
-	FOREIGN KEY(trainer_id) references trainer(trainer_id) ON DELETE CASCADE
-	FOREIGN KEY(pid) references pokemonName(pid) ON DELETE CASCADE);
+	FOREIGN KEY(trainer_id) references trainer,
+	FOREIGN KEY(pid) references pokemonName 
+		ON DELETE CASCADE);
 
 Create table professorTrades(
 	professor_name varchar(55) not null PRIMARY KEY,
 	pid int,
 	trainer_id int,
-	FOREIGN KEY(pid) references pokemonName(pid) ON DELETE CASCADE,
-	FOREIGN KEY(trainer_id) references trainer(trainer_id) ON DELETE CASCADE);
-
-Create table healPokemon(
-	pid int not null, 
-	nid int not null,
-	FOREIGN KEY(pid) references pokemonName(pid) ON DELETE CASCADE,
-	FOREIGN KEY(nid) references nurse(nid) ON DELETE CASCADE);
+	FOREIGN KEY(pid) references pokemonName 
+		ON DELETE CASCADE,
+	FOREIGN KEY(trainer_id) references trainer 
+		ON DELETE CASCADE);
 
 Create table nurse(
 	nid int not null PRIMARY KEY,
-	hname varchar(55),
-	FOREIGN KEY(hname) references Hospital(hname) ON DELETE CASCADE);
-
+	hname varchar(55));
 
 Create table hospital(
 	hname varchar(55) not null PRIMARY KEY,
 	nid int not null,
-	FOREIGN KEY(nid) references nurse(nid) ON DELETE CASCADE);
+	FOREIGN KEY(nid) references nurse
+		ON DELETE CASCADE);
+
+ALTER TABLE nurse
+ADD CONSTRAINT fk_hospital
+	FOREIGN KEY(hname) references hospital;
+
+create table healPokemon(
+	pid int not null,
+	nid int not null,
+	FOREIGN KEY(pid)
+		references pokemonName
+		ON DELETE CASCADE,
+	FOREIGN KEY(nid)
+		references nurse
+		ON DELETE CASCADE);
+
+Create table Incubator(
+	iid int not null PRIMARY KEY,
+	pid int,
+ 	FOREIGN KEY(pid) references pokemonName
+		ON DELETE CASCADE);
 
 Create table Incubation(
 	nid int NOT NULL,
 	iid int NOT NULL,
 	pid int NOT NULL, 
-	FOREIGN KEY(nid) references Nurse(nid) ON DELETE CASCADE,
-	FOREIGN KEY(iid) references Incubator(iid),
-	FOREIGN KEY(pid) references pokemonName(pid) ON DELETE CASCADE) ;
+	FOREIGN KEY(nid) references Nurse
+		ON DELETE CASCADE,
+	FOREIGN KEY(iid) references Incubator
+		ON DELETE CASCADE,
+	FOREIGN KEY(pid) references pokemonName
+		ON DELETE CASCADE);
 
-Create table Incubator(
-	iid int not null PRIMARY KEY,
-	pid int,
- 	FOREIGN KEY(pid) references pokemonName(pid) ON DELETE CASCADE);
 
 Create table eggDeposit( 
-	Eddate int not null PRIMARY KEY,
+	Eddate TIMESTAMP not null PRIMARY KEY,
 	Pid int not null,
 	trainer_id int not null,
-	FOREIGN KEY(pid) references pokemonName(pid) ON DELETE CASCADE,
-	FOREIGN KEY(trainer_id) references trainer(trainer_id) ON DELETE CASCADE);
+	FOREIGN KEY(pid) references pokemonName
+		ON DELETE CASCADE,
+	FOREIGN KEY(trainer_id) references trainer
+		ON DELETE CASCADE);
